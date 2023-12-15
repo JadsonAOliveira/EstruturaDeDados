c2a2a8976fed01b388ad205ee656688f87b9afb5
JadsonAOliveira <91754639+JadsonAOliveira@users.noreply.github.com>
2023-12-15 01:57:33 -0300

    Update Programa.java

--- estruturaDeDados/src/lista/Programa.java
+++ estruturaDeDados/src/lista/Programa.java
@@ -3,18 +3,32 @@ package lista;
 public class Programa {
     public static void main(String[] args) {
 
-        ListaLigada list = new ListaLigada();
-        list.add("SE");
-        list.add("MG");
-        list.add("BA");
-        list.add("RJ");
-        System.out.println("Tamanho: " + list.getSize());
-        System.out.println("Primeiro: " + list.getFirst().getValue());
-        System.out.println("Ultimo: " + list.getLast().getValue());
-        System.out.println(list.get(0).getValue());
-        System.out.println(list.get(1).getValue());
-        System.out.println(list.get(2).getValue());
-        System.out.println(list.get(3).getValue());
-    }
+        ListaLigada lista = new ListaLigada();
+        lista.adicionar("AC");
+        lista.adicionar("BA");
+        lista.adicionar("CE");
+        lista.adicionar("DF");
+        System.out.println("Tamanho: " + lista.getTamanho());
+        System.out.println("Primeiro: " + lista.getPrimeiro().getValor());
+        System.out.println("Ultimo: " + lista.getUltimo().getValor());
 
+        for (int i = 0; i < lista.getTamanho(); i++) {
+            System.out.println(lista.get(i).getValor());
+        }
+
+        // remover estado
+        lista.remover("DF");
+        System.out.println("removeu estado DF");
+        lista.adicionar("SP");
+        lista.remover("BA");
+        lista.remover("CE");
+        lista.remover("AC");
+        lista.remover("SP");
+        lista.adicionar("RJ");
+
+        System.out.println("Tamanho: " + lista.getTamanho());
+        for (int i = 0; i < lista.getTamanho(); i++) {
+            System.out.println(lista.get(i).getValor());
+        }
+    }
 }
